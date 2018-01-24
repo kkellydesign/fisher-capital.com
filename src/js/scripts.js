209733eb80b3b6e7aa2550fc90c4f644a1fa8d48
Kasey Kelly <kasey@servee.com>
2018-01-24 00:52:16 -0500

    subpage layouts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,6 +10,23 @@ $("document").ready(function () {
       scrolling   : 'no'
     });
 
+    $("[data-fancybox]").fancybox({
+      btnTpl : {
+       // Arrows
+       arrowLeft : '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
+                       '<svg viewBox="0 0 45 84" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">' +
+                         '<path d="M42.235 82.453L2.165 42.384l40.07-40.07" stroke-width="3" fill="transparent" stroke-linecap="round"/>' +
+                       '</svg>' +
+                     '</button>',
+
+       arrowRight : '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
+                     '<svg viewBox="0 0 45 84" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">' +
+                       '<path d="M2.375 2.314l40.069 40.07-40.07 40.07" stroke-width="3" fill="transparent" stroke-linecap="round"/>' +
+                     '</svg>' +
+                   '</button>'
+   },
+     });
+
     $(".nojs").remove();
     $('#contact').attr({'action': "/siteapi/submit_form"});
 });
